e4ee441d4e73644b7e84de42e8c3e8236d540b62
Kiovics Dániel <dkiovics@gmail.com>
2025-11-18 23:01:52 +0100

    value fixes

--- robonaut-firmware/Appli/HwDrivers/Servo/impl/servo.h
+++ robonaut-firmware/Appli/HwDrivers/Servo/impl/servo.h
@@ -6,8 +6,8 @@
 #include "tim.h"
 
 #define SERVO_TIMER &htim15
-#define SERVO_MIN_PULSE_WIDTH  1000  // in microseconds
-#define SERVO_MAX_PULSE_WIDTH  2000  // in microseconds
+#define SERVO_MIN_PULSE_WIDTH  1200  // in microseconds
+#define SERVO_MAX_PULSE_WIDTH  2050  // in microseconds
 #define SERVO_FRONT_CHANNEL    TIM_CHANNEL_1
 #define SERVO_BACK_CHANNEL     TIM_CHANNEL_2
 
