950181059f770c609debc6cb3d2b072a4703afdd
Andi Serban <andi.andrada@gmail.com>
2025-12-14 18:19:58 +0100

    IMU driver and init

--- robonaut-firmware/Appli/Control/System/impl/sys_init.c
+++ robonaut-firmware/Appli/Control/System/impl/sys_init.c
@@ -19,6 +19,7 @@
 #include "LineProcessor/line_interface.h"
 #include "ControllerTuning/tuning_interface.h"
 #include "Telemetry/tel_interface.h"
+#include "IMU/imu_interface.h"
 
 static drv_ControlParamsType telVar_currentDrvParams = {
     .P = 1.6f,
@@ -29,6 +30,8 @@ static drv_ControlParamsType telVar_currentDrvParams = {
 };
 static float telVar_currentMaxPower = 0.3f;
 static uint32_t telVar_maxEncoderCps = 100000; // 100k counts per second for now, TODO: tune properly
+
+static imu_Imu _sys_imuInstance;
 
 void sys_Init(void)
 {
@@ -46,6 +49,8 @@ void sys_Init(void)
     drv_Init(&telVar_currentDrvParams, telVar_currentMaxPower, telVar_maxEncoderCps);
     line_Init();
     tel_Init();
+    imu_init(&_sys_imuInstance, &hspi2, IMU_CS_GPIO_Port, IMU_CS_Pin, SPI2_IRQn, &htim18);
+    imu_setDefaultSettings(&_sys_imuInstance);
     
     //CTRL_InitLoop();
     //tuning_Init(&tuning_params);
