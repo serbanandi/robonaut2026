9d6f0df412beba9d9914992ac28ec9afb6b196dd
Andi Serban <andi.andrada@gmail.com>
2026-01-31 18:14:11 +0100

    fsbl shell script

---
+++ robonaut-firmware/Scripts/upload-fsbl.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BuildType=$1
+STM32ProgrammerPath=$2
+ProjectName=$3
+
+fsblBinPath="FSBL/build/$BuildType/robonaut-firmware_FSBL.bin"
+fsblTrustedBinPath="FSBL/build/$BuildType/robonaut-firmware_FSBL-trusted.bin"
+
+echo "y" | "$STM32ProgrammerPath/STM32_SigningTool_CLI.exe" \
+    -bin "$fsblBinPath" \
+    -nk \
+    -of 0x80000000 \
+    -t fsbl \
+    -o "$fsblTrustedBinPath" \
+    -hv 2.3 \
+    -dump "$fsblTrustedBinPath" \
+    -align
+
+"$STM32ProgrammerPath/STM32_Programmer_CLI" \
+    -c port=SWD mode=Normal ap=1 \
+    -w "FSBL/build/$BuildType/${ProjectName}_FSBL-trusted.bin" 0x70000000 \
+    -v \
+    -el MemLoader/RobonAUT_FSBL_XIP_ExtMemLoader.stldr \
+    -hardRst
